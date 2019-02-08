07a257f05e4928e63a5696ff4a59bcf9142413e6
Mridul Malviya <mridul.malviya@gmail.com>
2019-02-08 08:54:53 +0530

    Add project files.

---
+++ DemoTest/DemoTest/App.xaml.cs
@@ -0,0 +1,39 @@
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation (XamlCompilationOptions.Compile)]
+namespace DemoTest
+{
+	public partial class App : Application
+	{
+		public App ()
+		{
+			InitializeComponent();
+
+			MainPage = new MainPage();
+		}
+
+		protected override void OnStart ()
+		{
+            // Handle when your app starts
+            AppCenter.Start("android=f800fee1-7846-47c4-9f04-f858c993bc95;" +
+                   "uwp={Your UWP App secret here};" +
+                   "ios={Your iOS App secret here}",
+                   typeof(Analytics), typeof(Crashes));
+        }
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
---
+++ DemoTest/DemoTest/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DemoTest
+{
+	public partial class MainPage : ContentPage
+	{
+        private Object o;
+        string d = String.Empty;
+        int a, b, c;
+        public MainPage()
+		{
+			InitializeComponent();
+            o = new object();
+            Analytics.TrackEvent("Show method");
+
+
+            AppCenter.LogLevel = LogLevel.Verbose;
+        }
+
+
+        private void Show(int result)
+        {
+            Analytics.TrackEvent("Show method");
+            Console.WriteLine(result);
+        }
+
+        private int Add(int a, int b)
+        {
+            Analytics.TrackEvent("Add method");
+            return a + b;
+        }
+
+        
+
+        private void Btn_Clicked(object sender, EventArgs e)
+        {
+            Analytics.TrackEvent("Btn_Clicked");
+
+            var re=Add(1,2);
+            Show(re);
+
+            a = 0;b = 1;c = 0;
+            AppCenter.LogLevel = LogLevel.Debug;
+            AppCenter.LogLevel = LogLevel.Error;
+          //  c = b / a;
+            Console.WriteLine(c);
+        }
+    }
+}
